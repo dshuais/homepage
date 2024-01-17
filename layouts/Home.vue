e5e20053dd85e3bfb6fe97209251e72c9ba6617e
dshuais <1137896420@qq.com>
2024-01-17 18:20:37 +0800

    feat: 新增自定义layout

---
+++ layouts/Home.vue
@@ -0,0 +1,8 @@
+<script lang="ts" setup>
+
+</script>
+<template>
+  <div>
+    123
+  </div>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,19 @@
 export default defineNuxtConfig({
-  extends: '@nuxt-themes/docus'
+  extends: '@nuxt-themes/docus',
+  modules: ['@nuxtjs/tailwindcss'], // '@nuxtjs/plausible'
+  runtimeConfig: {
+    public: {
+      // plausible: {
+      //   domain: 'create-keep-design.dshuais.com',
+      // },
+      // algolia: {
+      //   applicationId: '',
+      //   apiKey: '',
+      //   langAttribute: 'lang',
+      //   docSearch: {
+      //     indexName: ''
+      //   }
+      // }
+    },
+  },
 })
