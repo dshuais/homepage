0ccd13cc9b5f3ebde9697eb20538060a89a664fd
dshuais <1137896420@qq.com>
2024-01-29 14:57:07 +0800

    fix: 优化card没有100%height的问题

--- components/home/Notes/card.tsx
+++ components/home/Notes/card.tsx
@@ -2,7 +2,7 @@
  * @Author: dushuai
  * @Date: 2024-01-28 17:37:23
  * @LastEditors: dushuai
- * @LastEditTime: 2024-01-29 09:56:01
+ * @LastEditTime: 2024-01-29 14:44:48
  * @Description: 笔记card
  */
 import styles from './index.module.css'
@@ -12,7 +12,7 @@ export default defineComponent({
   setup(props, { slots }) {
     return () => (
       <div class={['relative rounded-xl note-card', styles.note]}>
-        <div class={['relative z-10 rounded-xl bg-white dark:bg-pickled-bluewood-800', styles.mask]}>
+        <div class={['relative z-10 rounded-xl bg-white dark:bg-pickled-bluewood-800 h-full', styles.mask]}>
           {slots.default?.()}
         </div>
       </div>
