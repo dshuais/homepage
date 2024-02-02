e4d6e82e72fb25624f3baa443f8b96ca82ea6e11
dshuais <1137896420@qq.com>
2024-02-02 18:34:26 +0800

    feat: 新增lottie

--- components/home/Footer/index.tsx
+++ components/home/Footer/index.tsx
@@ -2,15 +2,30 @@
  * @Author: dushuai
  * @Date: 2024-02-02 17:33:55
  * @LastEditors: dushuai
- * @LastEditTime: 2024-02-02 18:14:03
+ * @LastEditTime: 2024-02-02 18:34:13
  * @description: HomeFooter
  */
+import Lottie from 'lottie-web'
+import Cat from '@/assets/resource/cat.json'
 
 export default defineComponent({
   name: 'HomeFooter',
   setup() {
+
+    const lottieDom = ref<HTMLElement>()
+
+    onMounted(() => {
+      Lottie.loadAnimation({
+        container: lottieDom.value as HTMLElement,
+        renderer: 'svg',
+        loop: true,
+        autoplay: true,
+        animationData: Cat,
+      })
+    })
+
     return () => (
-      <div class="mx-auto px-6 py-16 lg:flex lg:items-center lg:justify-between lg:px-8 lg:w-[85rem]">
+      <div data-aos="" class="mx-auto px-6 py-16 lg:flex lg:items-center lg:justify-between lg:px-8 lg:w-[85rem] relative">
         <h2 class="text-3xl font-bold tracking-tight text-gray-900 dark:text-white sm:text-4xl">
           I hope it can help you!
           <br />
@@ -26,6 +41,7 @@ export default defineComponent({
             class="text-sm font-semibold leading-6 text-gray-900 dark:text-white">Visit blog website <span
               aria-hidden="true">→</span></a>
         </div>
+        <div ref={lottieDom} class="absolute h-full ml-[-40px]"></div>
       </div>
     )
   }
