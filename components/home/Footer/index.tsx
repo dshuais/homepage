da34fdfd9d169f1394b1c6420004d2c74f4e485c
dshuais <1137896420@qq.com>
2024-02-02 18:11:09 +0800

    fix: 修改博客地址

--- components/home/Footer/index.tsx
+++ components/home/Footer/index.tsx
@@ -2,7 +2,7 @@
  * @Author: dushuai
  * @Date: 2024-02-02 17:33:55
  * @LastEditors: dushuai
- * @LastEditTime: 2024-02-02 18:00:03
+ * @LastEditTime: 2024-02-02 18:10:29
  * @description: HomeFooter
  */
 
@@ -18,10 +18,11 @@ export default defineComponent({
         </h2>
         <div class="mt-10 flex items-center gap-x-6 lg:mt-0 lg:flex-shrink-0">
           <a href="mailto:dsshuais2020@163.com"
+            target="_blank"
             class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
             Send me an email!
           </a>
-          <a href="https://www.sidestream.tech/" target="_blank"
+          <a href="https://blog.dshuais.com/" target="_blank"
             class="text-sm font-semibold leading-6 text-gray-900 dark:text-white">Visit blog website <span
               aria-hidden="true">→</span></a>
         </div>
