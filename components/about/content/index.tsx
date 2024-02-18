02bab3393216a718f6353a8d3acaef23238af48d
dshuais <1137896420@qq.com>
2024-02-18 10:51:06 +0800

    feat: 新增about content

---
+++ components/about/content/index.tsx
@@ -0,0 +1,18 @@
+/*
+ * @Author: dushuai
+ * @Date: 2024-02-18 10:29:49
+ * @LastEditors: dushuai
+ * @LastEditTime: 2024-02-18 10:49:31
+ * @description: about content
+ */
+export default defineComponent({
+  name: 'AboutContent',
+  setup() {
+
+    return () => (
+      <div class="w-full h-full bg-gray-500">
+        1
+      </div>
+    )
+  }
+})
--- layouts/About.tsx
+++ layouts/About.tsx
@@ -2,7 +2,7 @@
  * @Author: dushuai
  * @Date: 2024-02-04 10:42:33
  * @LastEditors: dushuai
- * @LastEditTime: 2024-02-05 22:51:09
+ * @LastEditTime: 2024-02-18 10:49:59
  * @description: 关于我
  */
 export default defineComponent({
@@ -12,8 +12,11 @@ export default defineComponent({
       // <div class="mx-auto py-16 lg:w-[85rem] px-3 lg:px-0">
       //   <NuxtImg class="mx-auto" src="https://svg-banners.vercel.app/api?type=origin&text1=Welcome&text2=dshuais%27%20homepage&width=800&height=300" alt="Name Image" />
       // </div>
-      <div>
+      <div class="lg:flex">
         <AboutSidebar />
+        <div class="mt-14 lg:mt-0 lg:w-[calc(100%-17rem)]">
+          <AboutContent />
+        </div>
       </div>
     )
   }
