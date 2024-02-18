c2cd6b6a2271398a8e791c1e339b0e4b4d546dbf
dshuais <1137896420@qq.com>
2024-02-18 17:37:24 +0800

    ✨ feat: test

--- components/about/introduce/index.tsx
+++ components/about/introduce/index.tsx
@@ -2,14 +2,14 @@
  * @Author: dushuai
  * @Date: 2024-02-18 10:57:04
  * @LastEditors: dushuai
- * @LastEditTime: 2024-02-18 11:34:22
+ * @LastEditTime: 2024-02-18 17:36:28
  * @description: AboutIntroduce
  */
 function AboutIntroduce() {
 
   return (
     <div class="h-[200vh]">
-      介绍
+      介绍 test
     </div>
   )
 }
