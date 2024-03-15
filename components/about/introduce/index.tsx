5158fb247e9414aeb5583050f2b5e23f560ea775
dshuais <1137896420@qq.com>
2024-03-15 17:52:07 +0800

    🐞 fix: 修改简历形容

--- components/about/introduce/index.tsx
+++ components/about/introduce/index.tsx
@@ -2,14 +2,14 @@
  * @Author: dushuai
  * @Date: 2024-02-18 10:57:04
  * @LastEditors: dushuai
- * @LastEditTime: 2024-02-18 17:36:28
+ * @LastEditTime: 2024-03-15 17:51:03
  * @description: AboutIntroduce
  */
 function AboutIntroduce() {
 
   return (
     <div class="h-[200vh]">
-      介绍 test
+      个人简历 开发中
     </div>
   )
 }
